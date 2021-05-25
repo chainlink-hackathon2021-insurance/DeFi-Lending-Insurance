98aa9beefe8b5b841d2e067055874fa12375fe6c
cenobyte321 <aramirezagu@gmail.com>
2021-05-25 06:01:54 -0500

    Removed unused views

--- packages/react-app/src/views/index.js
+++ packages/react-app/src/views/index.js
@@ -1,5 +1,3 @@
-export { default as Hints } from "./Hints";
-export { default as ExampleUI } from "./ExampleUI";
 export { default as Subgraph } from "./Subgraph";
 export { default as RegistrationSuccess } from "./RegistrationSuccess";
 export { default as SmartContractDetails } from "./SmartContractDetails"; 
